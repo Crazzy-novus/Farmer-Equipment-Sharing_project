db5b9982e57372ea6bddd0cd0c66d39fc82132bd
Crazzy-novus <duraivignesh222@gmail.com>
2023-07-10 23:01:03 +0530

    partial authentication
    Signed-off-by: Crazzy-novus <duraivignesh222@gmail.com>

--- FramerFriend/app/src/main/java/com/example/framerfriend/LoginActivity.java
+++ FramerFriend/app/src/main/java/com/example/framerfriend/LoginActivity.java
@@ -1,14 +1,76 @@
 package com.example.framerfriend;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // declaration
+    TextInputEditText Email_tf, Password_tf;
+    Button Login_bt;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialization
+        mAuth = FirebaseAuth.getInstance();
+        Email_tf = findViewById(R.id.email_tf);
+        Password_tf = findViewById(R.id.password_tf);
+        Login_bt = findViewById(R.id.login_bt);
+
+        Login_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // read text from edit text field
+
+                String email,password;
+                email = String.valueOf(Email_tf.getText());
+                password = String.valueOf(Password_tf.getText());
+
+                // email and password is empty or not
+                // Show toast message on screen
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(LoginActivity.this, "Enter the email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter the password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, "Account Created",
+                                            Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
